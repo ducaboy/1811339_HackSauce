f2460bf8a1781435081f180a6e3d4c881c0d8885
Francesco <galle2105@gmail.com>
2026-03-10 12:26:01 +0100

    updated initial rules

--- source/processing-service/src/main/java/com/hacksause/processing/processingservice/DataInitializer.java
+++ source/processing-service/src/main/java/com/hacksause/processing/processingservice/DataInitializer.java
@@ -19,10 +19,13 @@ public class DataInitializer implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         if (ruleRepository.count() > 0) return; // don't re-seed if rules already exist
 
-        ruleRepository.save(new Rule("greenhouse_temperature", "temperature_c", ">", 30.0, "cooling_fan", "ON"));
-        ruleRepository.save(new Rule("greenhouse_temperature", "temperature_c", "<", 18.0, "habitat_heater", "ON"));
-        ruleRepository.save(new Rule("entrance_humidity", "humidity_pct", "<", 25.0, "entrance_humidifier", "ON"));
-        ruleRepository.save(new Rule("entrance_humidity", "humidity_pct", ">", 70.0, "entrance_humidifier", "OFF"));
-        ruleRepository.save(new Rule("co2_hall", "co2_ppm", ">", 1200.0, "hall_ventilation", "ON"));
+        ruleRepository.save(new Rule("greenhouse_temperature", "temperature_c", ">", 25.0, "cooling_fan", "ON"));
+        ruleRepository.save(new Rule("greenhouse_temperature", "temperature_c", "<", 24.0, "cooling_fan", "OFF"));
+        ruleRepository.save(new Rule("greenhouse_temperature", "temperature_c", "<", 24.0, "habitat_heater", "ON"));
+        ruleRepository.save(new Rule("greenhouse_temperature", "temperature_c", ">", 25.0, "habitat_heater", "OFF"));
+        ruleRepository.save(new Rule("entrance_humidity", "humidity_pct", "<", 38.0, "entrance_humidifier", "ON"));
+        ruleRepository.save(new Rule("entrance_humidity", "humidity_pct", ">", 39.0, "entrance_humidifier", "OFF"));
+        ruleRepository.save(new Rule("co2_hall", "co2_ppm", ">", 780.0, "hall_ventilation", "ON"));
+        ruleRepository.save(new Rule("co2_hall", "co2_ppm", "<", 779.0, "hall_ventilation", "OFF"));
     }
 }
